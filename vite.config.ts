c43aa68e177147975bd2dcce85daf835a8c647f3
Per <per@stuffmatic.com>
2024-09-29 06:54:29 +0200

    use https for dev server to make copy to clipboard work on iOS safari

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    basicSsl(), // https needed for copying to clipboard on some browsers
+    react()
+  ],
 })
