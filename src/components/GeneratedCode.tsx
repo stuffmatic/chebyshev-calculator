0f6ecb1efd4e87e7e925d3e6225f92a14da3bfe8
Per <per@stuffmatic.com>
2024-09-29 13:54:27 +0200

    nicer code copy notification bubble

--- src/components/GeneratedCode.tsx
+++ src/components/GeneratedCode.tsx
@@ -5,24 +5,51 @@ import Button from "antd/lib/button"
 import Select from "antd/lib/select"
 import { ScrollableContent } from "./ScrollableContent"
 import { ChebyshevExpansion } from "../util/chebyshev-expansion"
+import SuccessIcon from './../assets/icon_check.svg'
+import ErroIcon from './../assets/icon_error.svg'
 
 const playgroundUrl = (language: TargetLanguage): string | undefined => {
     switch (language) {
         case TargetLanguage.c:
             return "https://programiz.pro/ide/c"
         case TargetLanguage.python:
-            return "https://playground.programiz.com/"
+            return "https://www.online-python.com/"
         case TargetLanguage.rust:
             return "https://play.rust-lang.org/?version=stable&mode=debug&edition=2021"
     }
+}
+
+const ResultIcon = (props: { success: boolean }) => {
+    return <div style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        left: "100%", top: "0%",
+        transform: "translate(-50%, -50%)",
+        width: "20px",
+        height: "20px",
+        borderRadius: "10px",
+        backgroundColor: props.success ? "#52AE1F" : "red"
+    }}><img style={{ width: "16px" }} src={props.success ? SuccessIcon : ErroIcon} /></div>
 }
 
 export const GeneratedCode = (props: { expansion: ChebyshevExpansion }) => {
     const [targetLanguage, setTargetLanguage] = useState<TargetLanguage>(TargetLanguage.c)
     const codeSnippet = generateCode(targetLanguage, props.expansion)
     const liveUrl = playgroundUrl(targetLanguage)
-    const [donePopupVisible, setDonePopupVisible] = useState(false)
+    const [donePopupResult, setDonePopupResult] = useState<boolean | null>(null)
     const donePopupTimeout = useRef<number | null>(null)
+
+    const showResultIcon = (success: boolean) => {
+        if (donePopupTimeout.current) {
+            clearTimeout(donePopupTimeout.current)
+        }
+        setDonePopupResult(success)
+        donePopupTimeout.current = setTimeout(() => {
+            setDonePopupResult(null);
+        }, 1600)
+    }
 
     return <>
         <div style={{ display: "flex", width: "100%", alignItems: "center" }}>
@@ -40,19 +67,19 @@ export const GeneratedCode = (props: { expansion: ChebyshevExpansion }) => {
                     })
                 } />
 
-            <Tooltip title="Done!" open={donePopupVisible} placement="top" color="#52AE1F">
-                <Button style={{ marginLeft: "10px" }} onClick={() => {
+            <div style={{ marginLeft: "10px", position: "relative" }}>
+                <Button onClick={() => {
+                    setDonePopupResult(null);
                     ((window.navigator as any).clipboard as any).writeText(codeSnippet).then(() => {
-                        if (donePopupTimeout.current) {
-                            clearTimeout(donePopupTimeout.current)
-                        }
-                        setDonePopupVisible(true)
-                        donePopupTimeout.current = setTimeout(() => {
-                            setDonePopupVisible(false)
-                        }, 1600)
+                        showResultIcon(true)
+                    }).catch((e: any) => {
+                        showResultIcon(false)
                     })
                 }}>Copy code</Button>
-            </Tooltip>
+                {
+                    donePopupResult != null ? <ResultIcon success={donePopupResult} ></ResultIcon> : null
+                }
+            </div>
         </div>
         {
             liveUrl !== undefined ?
